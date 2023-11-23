2d2606af174b92d32b5c8c2d5ac67b6a3d674f4a
Aditya <adityaratan63@gmail.com>
2023-11-23 10:43:14 +0530

    second commit
    Signed-off-by: Aditya <adityaratan63@gmail.com>

---
+++ 2.Composition/Customer.java
@@ -0,0 +1,44 @@
+/**
+ * Customer
+ */
+public class Customer {
+
+    int id;
+    String name;
+    int discount;
+
+    Customer(int id, String name, int discount) {
+        this.id = id;
+        this.name = name;
+        this.discount = discount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public String toString() {
+        return "name" + id + " " + "discount" + discount + "%";
+    }
+
+}
---
+++ 2.Composition/Invoice.java
@@ -0,0 +1,55 @@
+public class Invoice {
+    int id;
+    Customer customer;
+    double amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Invoice(int id, Customer customer, double amount) {
+        this.id = id;
+        this.customer = customer;
+        this.amount = amount;
+    }
+
+    public String getCustomerName() {
+        return customer.getName();
+    }
+
+    public int getCustomerId() {
+        return customer.getId();
+    }
+
+    public int getCustomerDiscount() {
+        return customer.getDiscount();
+    }
+
+    public double getAmountAfterDiscount() {
+        return amount - (customer.getDiscount() * amount) / 100;
+    }
+
+    public String toString() {
+        return "Invoice[" + id + " " + customer.toString() + ",amount=" + amount + "]";
+    }
+}
